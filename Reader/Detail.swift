67273de4ba89f1aa07c82b5f4d37ffbf3546c158
rockyshen <875430544@qq.com>
2025-01-21 23:25:25 +0800

    feat：完成Reader主界面、Detail界面开发；完成URLSession联调后端接口

---
+++ Reader/Detail.swift
@@ -0,0 +1,18 @@
+//
+//  Detail.swift
+//  Reader
+//
+//  Created by 沈俊杰 on 2025/1/21.
+//
+
+import SwiftUI
+
+struct Detail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Detail()
+}
