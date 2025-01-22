709d81ac60c7766cd162efb8088764b0ed44ad1c
rockyshen <875430544@qq.com>
2025-01-22 14:32:41 +0800

    feat：增加导航页的深色模式切换；切换按钮提取到Setting视图；Vibration.swift中声明震动反馈的枚举

---
+++ Reader/Setting.swift
@@ -0,0 +1,18 @@
+//
+//  Setting.swift
+//  Reader
+//
+//  Created by 沈俊杰 on 2025/1/22.
+//
+
+import SwiftUI
+
+struct Setting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Setting()
+}
---
+++ Reader/Vibration.swift
@@ -0,0 +1,18 @@
+//
+//  Vibration.swift
+//  Reader
+//
+//  Created by 沈俊杰 on 2025/1/22.
+//
+
+import SwiftUI
+
+struct Vibration: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Vibration()
+}
